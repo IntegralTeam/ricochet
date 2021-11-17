8fef970510c5fb1395511a6081857bb1f3f2389a
IntegralTeam O <repo@sfxdx.ru>
2021-11-18 01:04:14 +0300

    fixes to python script

--- tests/deploy_hardhat_test.py
+++ tests/deploy_hardhat_test.py
@@ -1,19 +1,25 @@
 import subprocess
+import os
 
 
 def deploy_hardhat_test():
-    copy_contracts_process = subprocess.Popen([
-        "cp", "-a", "./contracts", "./ricochet-hardhat/copied_contracts"
-    ], stdout=subprocess.STDOUT)
+    if not os.path.isdir("./ricochet-hardhat/copied_contracts"):
+        subprocess.run([
+            "mkdir", "./ricochet-hardhat/copied_contracts"
+        ], stdout=subprocess.STDOUT, capture_output=True)
 
-    deploy_hardhat_tests = subprocess.Popen([
+    subprocess.run([
+        "cp", "-a", "./contracts", "./ricochet-hardhat/copied_contracts"
+    ], stdout=subprocess.STDOUT, capture_output=True)
+
+    subprocess.run([
         "cd", "./ricochet-hardhat",
         "&&",
         "npx", "hardhat", "test",
         "&&",
         "cd", ".."
-    ], stdout=subprocess.STDOUT)
+    ], stdout=subprocess.STDOUT, capture_output=True)
 
-    remove_contracts_process = subprocess.Popen([
+    subprocess.run([
         "rm", "-rf", "./ricochet-hardhat/copied_contracts"
-    ], stdout=subprocess.STDOUT)
+    ], stdout=subprocess.STDOUT, capture_output=True)
